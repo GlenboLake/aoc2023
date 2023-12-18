d9a65522971c91b7017391bdfe652bdfc29c8d03
Glen Bradley <glenbolake@gmail.com>
2023-12-18 14:49:12 -0500

    Day 14 part 2

--- day14.py
+++ day14.py
@@ -1,20 +1,55 @@
+def rotate_string_ccw(s):
+    transposed = [''.join(row) for row in zip(*s.splitlines())]
+    flipped = reversed(transposed)
+    return '\n'.join(flipped)
+
+
+def rotate_string_cw(s):
+    flipped = reversed(s.splitlines())
+    transposed = [''.join(row) for row in zip(*flipped)]
+    return '\n'.join(transposed)
+
+
 def part1(filename):
     with open(filename) as f:
-        # Transpose the grid so we can push them to the left with str.replace
-        grid = '\n'.join(''.join(row) for row in zip(*f.read().splitlines()))
+        # Rotate input so north is to the left
+        grid = rotate_string_ccw(f.read())
     while '.O' in grid:
         grid = grid.replace('.O', 'O.')
-    # Transpose again to make enumerating easier
-    grid = list(zip(*grid.splitlines()))[::-1]
-    return sum(i * row.count('O') for i, row in enumerate(grid, start=1))
+    # Rotate CCW once more so north is at the bottom to make enumerating easier
+    grid = rotate_string_ccw(grid)
+    return sum(i * row.count('O') for i, row in enumerate(grid.splitlines(), start=1))
 
 
 def part2(filename):
-    pass
+    with open(filename) as f:
+        grid = f.read()
+    # Rotate 180 to prepare rotation
+    grid = rotate_string_cw(rotate_string_cw(grid))
+    history = {grid: 0}
+    for i in range(1, 1_000_000_001):
+        for _ in range(4):
+            grid = rotate_string_cw(grid)
+            while '.O' in grid:
+                grid = grid.replace('.O', 'O.')
+        if grid in history:
+            break
+        history[grid] = i
+    cycle_start = history[grid]
+    cycle_length = i - cycle_start
+    result = 1_000_000_000 % cycle_length
+    while result < cycle_start:
+        result += cycle_length
+    assert result in history.values()
+    final_grid = {k for k, v in history.items() if v == result}.pop()
+    return sum(i * row.count('O') for i, row in enumerate(final_grid.splitlines(), start=1))
 
 
 if __name__ == '__main__':
+    assert rotate_string_ccw('ab\ncd') == 'bd\nac'
+    assert rotate_string_cw('ab\ncd') == 'ca\ndb'
+    assert rotate_string_cw(rotate_string_ccw('ab\ncd')) == 'ab\ncd'
     assert part1('inputs/sample14.txt') == 136
     print('Part 1:', part1('inputs/day14.txt'))
-    assert part2('inputs/sample14.txt') == 0
+    assert part2('inputs/sample14.txt') == 64
     print('Part 2:', part2('inputs/day14.txt'))
